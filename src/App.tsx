761d71350567a7b281efe98e3626884c19838756
Humayun Wahla <humayunwahla55@gmail.com>
2024-10-08 09:50:41 +0500

    create app

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+   <div>
+    <h1 className="text-3xl font-bold underline bg-blue-600 rounded-xl px-3 py-3 text-white shadow-xl">
+      Hello world!
+    </h1>
+   </div>
+  )
+}
+
+export default App
